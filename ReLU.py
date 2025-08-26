521b5f230158b14156c06cbd75d1de9b3c3afbc7
Willow Otto <pz8313ru@go.minnsate.edu>
2025-08-26 15:33:26 -0500

    ReLU function

---
+++ ReLU.py
@@ -0,0 +1,10 @@
+def ReLU(number):
+    #if number is positive return number
+    #else, return 0!
+    if number > 0:
+        return number
+    else:
+        return 0
+
+print(ReLU(76)) #prints number
+print(ReLU(-100)) #prints 0
